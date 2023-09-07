73e261dca0df9f39323a4cd69d2ce5f48c69d7d2
Tomasz Dębosz <98646427+Dosztosz@users.noreply.github.com>
2023-09-07 22:09:46 +0200

    up

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ let temp = document.querySelector('.temp');
 let desc = document.querySelector('.desc');
 let humidity = document.querySelector('.humidity');
 let feelslike = document.querySelector('.feelslike');
+let image = document.querySelector('img')
 
 
 // ADDING EVENT LISTENER TO SEARCH BUTTON  
@@ -25,6 +26,7 @@ const displayData=(weather)=>{
     temp.innerText=`${weather.current.temp_c}°C`
     feelslike.innerText=`${weather.current.feelslike_c}°C`
     humidity.innerText=`${weather.current.humidity}%`
+    image.src=`${weather.current.condition.icon}`
     
 
 }
