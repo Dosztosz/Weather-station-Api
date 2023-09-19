1d3a29bb5492b81a0b6624e9a5e165273d47315f
Dosztosz <dosztosz@int.pl>
2023-09-19 14:43:36 +0200

    large update
    In this Update I have added a forecast weather and a simple nwes system

--- script.js
+++ script.js
@@ -6,19 +6,28 @@ let temp = document.querySelector('.temp');
 let desc = document.querySelector('.desc');
 let humidity = document.querySelector('.humidity');
 let feelslike = document.querySelector('.feelslike');
-let image = document.querySelector('img')
+let image = document.querySelector('img');
+let futureWeather = document.querySelector('.forecast');
+var tbody = document.getElementById('tbody');
+const newHour = {};
+
 
 
 // ADDING EVENT LISTENER TO SEARCH BUTTON  
 function showWeather(){
 
     // Fection data from open weather API
-    fetch(`http://api.weatherapi.com/v1/current.json?key=5bc8c728be8d4178ba2123115230709&q=Bochnia&aqi=no`)
+    fetch(`http://api.weatherapi.com/v1/forecast.json?key=5bc8c728be8d4178ba2123115230709&q&q&q&q=Bochnia&days=1&aqi=yes&alerts=yes`)
     .then(response => response.json())
-    .then(
-        displayData)
-    .catch(err => alert('Wrong City name')); 
+    .then(displayData);
 
+}
+
+
+function showInformations(){
+    fetch(`https://newsapi.org/v2/top-headlines?country=pl&category=business&apiKey=2f9453099bc64564afee30ad1cb5d245`)
+    .then(response => response.json())
+    .then(displayInfo);
 }
 
 // Function to diplay weather on html document
@@ -27,9 +36,40 @@ const displayData=(weather)=>{
     feelslike.innerText=`${weather.current.feelslike_c}°C`
     humidity.innerText=`${weather.current.humidity}%`
     image.src=`${weather.current.condition.icon}`
-    
+    for(i = 0; i<=23; i++){
+        newHour[i] = {
+            hour: weather.forecast.forecastday[0].hour[i].time,
+            temp: weather.forecast.forecastday[0].hour[i].feelslike_c,
+            weather: weather.forecast.forecastday[0].hour[i].condition.text,
+            rain: weather.forecast.forecastday[0].hour[i].chance_of_rain
+
+        }
+        
+    }
+    var k = '<tbody>'
+    for(i = 0;i <= 23 ; i = i+2){
+        k+= '<tr>';
+        k+= '<td>' + newHour[i].hour.substring(10) + '</td>';
+        k+= '<td>' + newHour[i].temp + '°C</td>';
+        k+= '<td>' + newHour[i].weather + '</td>';
+        k+= '<td>' + newHour[i].rain + '%</td>';
+        k+= '</tr>';
+    }
+    k+='</tbody>';
+    document.getElementById('tableData').innerHTML = k;
+}
+
+// Function to display information on html document
+
+const displayInfo=(articles)=>{
+
+    let articleTitle = `${articles.articles[0].title}`;
+    let articleUrl = `${articles.articles[0].url}`;
+    document.getElementById('headline').innerHTML = `<a href="${articleUrl}"><h2>`+articleTitle+`</h2></a>`;
+    console.log(articles.totalResults)
 
 }
 
     
 showWeather()
+showInformations()
